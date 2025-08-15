chucklever/ktls-utils

commit d1e87171d961fcd86bda6cf67cfb4c63a0df3ac2

tlshd: Disable Nagle during handshakes on TCP sockets
Fedora Docs suggest:
> The TLS handshake has very poor performance if the TCP Nagle
> algorithm is active. You should switch on the TCP_NODELAY socket
> option (at least for the duration of the handshake), or use the
> Linux-specific TCP_CORK option.

Signed-off-by: Chuck Lever <chuck.lever@oracle.com>

src/tlshd/handshake.c
@@ -43,6 +43,34 @@
 #include "tlshd.h"
 #include "netlink.h"
 
+static void tlshd_set_nagle(gnutls_session_t session, int val)
+{
+	int ret;
+
+	ret = setsockopt(gnutls_transport_get_int(session),
+			 IPPROTO_TCP, TCP_NODELAY, &val, sizeof(val));
+	if (ret < 0)
+		tlshd_log_perror("setsockopt (NODELAY)");
+}
+
+static void tlshd_save_nagle(gnutls_session_t session, int *saved)
+{
+	socklen_t len;
+	int ret;
+
+
+	len = sizeof(saved);
+	ret = getsockopt(gnutls_transport_get_int(session),
+			 IPPROTO_TCP, TCP_NODELAY, saved, &len);
+	if (ret < 0) {
+		tlshd_log_perror("getsockopt (NODELAY)");
+		saved = 0;
+		return;
+	}
+
+	tlshd_set_nagle(session, 1);
+}
+
 /**
  * tlshd_start_tls_handshake - Drive the handshake interaction
  * @session: TLS session to initialize
@@ -53,8 +81,8 @@ void tlshd_start_tls_handshake(gnutls_session_t session,
 			       struct tlshd_handshake_parms *parms)
 {
 	char *priorities;
+	int saved, ret;
 	char *desc;
-	int ret;
 
 	priorities = tlshd_make_priorities_string(parms);
 	if (priorities) {
@@ -77,9 +105,11 @@ void tlshd_start_tls_handshake(gnutls_session_t session,
 	}
 
 	gnutls_handshake_set_timeout(session, parms->timeout_ms);
+	tlshd_save_nagle(session, &saved);
 	do {
 		ret = gnutls_handshake(session);
 	} while (ret < 0 && !gnutls_error_is_fatal(ret));
+	tlshd_set_nagle(session, saved);
 	if (ret < 0) {
 		switch (ret) {
 		case GNUTLS_E_CERTIFICATE_VERIFICATION_ERROR:

commit 6e5c8eef4833338bafd9f97424502f0c920d632a

tlshd: Address Cppcheck complaints in handshake.c
src/tlshd/handshake.c:67:3: warning: Assignment of function parameter has no effect outside the function. Did you forget dereferencing it? [uselessAssignmentPtrArg]
  saved = 0;
  ^
src/tlshd/handshake.c:67:9: style: Variable 'saved' is assigned a value that is never used. [unreadVariable]
  saved = 0;
        ^

Fixes: d1e87171d961 ("tlshd: Disable Nagle during handshakes on TCP sockets")
Signed-off-by: Chuck Lever <chuck.lever@oracle.com>

src/tlshd/handshake.c
@@ -64,7 +64,7 @@ static void tlshd_save_nagle(gnutls_session_t session, int *saved)
 			 IPPROTO_TCP, TCP_NODELAY, saved, &len);
 	if (ret < 0) {
 		tlshd_log_perror("getsockopt (NODELAY)");
-		saved = 0;
+		*saved = 0;
 		return;
 	}
 