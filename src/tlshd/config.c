e091916d054e7e40fc1ee03c8e5ed30549518ceb
Chuck Lever <chuck.lever@oracle.com>
2025-09-02 22:33:04 -0400

    Build Doxygen web site
    Make use of the nice documenting comments I've added over the
    years to build some developer documentation.
    
    The generated pages are not installed (but could be if there is
    demand).
    
    Signed-off-by: Chuck Lever <chuck.lever@oracle.com>

--- src/tlshd/config.c
+++ src/tlshd/config.c
@@ -49,6 +49,18 @@
 #include "tlshd.h"
 
 /**
+ * @page config Configuration
+ *
+ * @section man Man pages
+ * @subsection tlshd_conf_5 tlshd.conf.5
+ * @htmlinclude tlshd.conf.5.html
+ *
+ * @section config Configuration examples
+ * @subsection tlshd_conf /etc/tlshd/config
+ * @verbinclude config
+ */
+
+/**
  * @var tlshd_configuration
  * @brief In-memory parsed config file
  */
